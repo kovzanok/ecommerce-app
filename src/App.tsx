cf2e1d1527645bd3f7a9e8ebe53b3bd288c28901
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-11 21:22:58 +0300

    [EA-73] feat: add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import LoginPage from './pages/login-page';
+import Layout from './components/Layout';
+
 function App() {
-  return <div></div>
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
