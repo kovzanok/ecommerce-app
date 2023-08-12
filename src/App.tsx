8d91a1b168f9536a41afcf52ec3e425064cdab49
Alex <105801205+AlexeiIsProgrammer@users.noreply.github.com>
2023-08-12 17:18:12 +0300

    [EA-68] Add register page (#19)
    * [EA-72] Add React Icon Pack (#12)
    
    * feat: add react-icon dependency
    
    * [EA-72] feat: add react-icon dependency
    
    * [EA-67] Add login page
    
    * [EA-67] feat: add login page
    
    * [EA-67] refactor: update method name
    
    * [EA-67] refactor: update file path
    
    * [EA-73] feat: install react router dom
    
    * [EA-73] feat: add layout
    
    * [EA-73] feat: add browser router
    
    * [EA-41] feat: add image
    
    * [EA-41] feat: add not found page
    
    * [EA-41] feat: add page to router
    
    ---------
    
    Co-authored-by: theNickola <110473056+theNickola@users.noreply.github.com>
    Co-authored-by: Alex Kovzan <alexanderkovzan@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import LoginPage from './pages/login-page';
+import Layout from './components/Layout';
+import NotFoundPage from './pages/not-found-page';
+
 function App() {
-  return <div></div>
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
