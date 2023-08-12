d69d5d8a602f7fb9c91ce7f2f7956d15bddf25c8
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-12 11:17:40 +0300

    [EA-28] feat: change user state type

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,9 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { CustomerSignInResult } from '@commercetools/platform-sdk';
 
-type UserState = CustomerSignInResult | null;
+type UserState = {
+  user: CustomerSignInResult | null;
+  loading: boolean;
+  error: string;
+};
 
-const initialState: UserState = null;
+const initialState: UserState = {
+  user: null,
+  loading: false,
+  error: '',
+};
 
 const userSlice = createSlice({
   name: 'user',
