2262ff3ccd2d4bb01cd3ca7e2de80bde54bcc2a1
theNickola <kurilchik.nv@gmail.com>
2023-08-14 00:39:15 +0300

    [EA-22] feat: add email validation rules

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -6,8 +6,33 @@ import {
   Flex,
   Title,
 } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 export default function LoginPage() {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+
+    validate: {
+      email: (value) => {
+        switch (false) {
+          case value.trim() === value:
+            return 'Email address must not contain leading or trailing whitespace';
+          case value.includes('@'):
+            return 'Email address must contain an "@" symbol separating local part and domain name';
+          case value.split('@')[1].split('.').length === 2:
+            return 'Email address must contain a domain name';
+          case /^\S+@\S+$/.test(value):
+            return 'Email address must be properly formatted';
+          default:
+            return '';
+        }
+      },
+    },
+  });
+
   return (
     <Center h="100vh">
       <Flex direction="column" gap="lg">
@@ -15,6 +40,7 @@ export default function LoginPage() {
           Login
         </Title>
         <form
+          onSubmit={form.onSubmit((values) => console.log(values))}
           style={{
             border: '1px solid orange',
             borderRadius: '5px',
@@ -22,8 +48,17 @@ export default function LoginPage() {
           }}
         >
           <Flex p="20px" direction="column" gap="lg">
-            <TextInput withAsterisk label="Email" />
-            <PasswordInput withAsterisk label="Password" />
+            <TextInput
+              withAsterisk
+              label="Email"
+              placeholder="your@email.com"
+              {...form.getInputProps('email')}
+            />
+            <PasswordInput
+              withAsterisk
+              label="Password"
+              {...form.getInputProps('password')}
+            />
             <Button type="submit" m="auto" w="40%" color="orange" size="md">
               Sign in
             </Button>
