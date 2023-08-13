c447337017b5fc8f1705698e2ceacc4e394878e0
theNickola <kurilchik.nv@gmail.com>
2023-08-14 01:23:03 +0300

    [EA-22] feat: add password validation rules

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -24,10 +24,24 @@ export default function LoginPage() {
             return 'Email address must contain an "@" symbol separating local part and domain name';
           case value.split('@')[1].split('.').length === 2:
             return 'Email address must contain a domain name';
-          case /^\S+@\S+$/.test(value):
+          case /^\S+@\S.\S+$/.test(value):
             return 'Email address must be properly formatted';
-          default:
-            return '';
+        }
+      },
+      password: (value) => {
+        switch (false) {
+          case value.length >= 8:
+            return 'Password must be at least 8 characters long';
+          case value.trim() === value:
+            return 'Email address must not contain leading or trailing whitespace';
+          case /[A-Z]/.test(value):
+            return 'Password must contain at least one uppercase letter (A-Z)';
+          case /[a-z]/.test(value):
+            return 'Password must contain at least one lowercase letter (a-z)';
+          case /[0-1]/.test(value):
+            return 'Password must contain at least one digit (0-9)';
+          case /!|@|#|\$|%|\^|&|\*/.test(value):
+            return 'Password must contain at least one special character (e.g., !@#$%^&*)';
         }
       },
     },
@@ -40,7 +54,7 @@ export default function LoginPage() {
           Login
         </Title>
         <form
-          onSubmit={form.onSubmit((values) => console.log(values))}
+          onSubmit={form.onSubmit((values) => values)}
           style={{
             border: '1px solid orange',
             borderRadius: '5px',
