e34d87480a2d290d3c737664457a911190ff457f
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-13 21:37:59 +0300

    [EA-44] feat: add button to logout

--- src/components/custom-header.tsx
+++ src/components/custom-header.tsx
@@ -1,17 +1,35 @@
 import {
-  Header, Image, Flex, Container,
+  Header, Image, Flex, Container, Button,
 } from '@mantine/core';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import { useAppDispatch, useAppSelector } from '../hooks';
+import userSelector from '../store/selectors';
+import { logout } from '../store/slices/userSlice';
+import AuthModule from '../service/modules/auth-module';
 
 export default function CustomHeader() {
+  const { user } = useAppSelector(userSelector);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(logout());
+    AuthModule.creatAnonymousApiRoot();
+    navigate('/login');
+  };
   return (
     <Header height={60}>
       <Container h="100%" size="xl">
-        <Flex h="100%" align="center">
+        <Flex h="100%" align="center" justify="space-between">
           <NavLink to="/">
             <Image width={50} height={50} src={logo} />
           </NavLink>
+          {user && (
+            <Button onClick={handleClick} variant="outline" color="orange">
+              Logout
+            </Button>
+          )}
         </Flex>
       </Container>
     </Header>
