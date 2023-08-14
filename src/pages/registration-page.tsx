e58ef2e8d53260c09c1fb6ad58e169c67e611672
Alexei <avenge.now.r@mail.ru>
2023-08-14 19:16:21 +0300

    [ЕА-30] feat: add some validation, install postal code libraries

--- src/pages/registration-page.tsx
+++ src/pages/registration-page.tsx
@@ -18,9 +18,97 @@ import { useDisclosure } from '@mantine/hooks';
 import { React, useState } from 'react';
 
 function Registration() {
+  const SPECIAL_CHARACTERS_REGULAR = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/;
+  const EMAIL_REGULAR = /^\S+@\S+$/;
+  const DIGIT_REGULAR = /\d/;
+  const LOWER_CASE_REGULAR = /[a-z]/;
+  const PASSWORD_MIN_LENGTH = 8;
+  const AGE_REQUIREMENT = 13;
+
+  const CURRENT_AGE = (dateString: string): number => {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age -= 1;
+    }
+    return age;
+  };
+
+  const stringValidation = (val: string): ValidationFunc => {
+    if (val.length === 0) {
+      return 'This field must contain at least one character';
+    }
+
+    if (SPECIAL_CHARACTERS_REGULAR.test(val)) {
+      return 'This field must contain no special characters or numbers';
+    }
+
+    return null;
+  };
+
+  const streetValidation = (val: string): ValidationFunc => {
+    if (val.length === 0) {
+      return 'This field must contain at least one character';
+    }
+
+    return null;
+  };
+
+  const emailValidation = (val: string): ValidationFunc => {
+    if (!EMAIL_REGULAR.test(val)) {
+      return 'This field must have a properly formatted email address (e.g., example@email.com)';
+    }
+
+    return null;
+  };
+
+  const passwordValidation = (val: string): ValidationFunc => {
+    if (val.length < PASSWORD_MIN_LENGTH) {
+      return 'Password must have minimum 8 characters';
+    }
+
+    if (!LOWER_CASE_REGULAR.test(val)) {
+      return 'Password must have at least 1 lowercase letter';
+    }
+
+    if (!DIGIT_REGULAR.test(val)) {
+      return 'Password must have at least one number';
+    }
+
+    return null;
+  };
+
+  const birthdayValidation = (val: string): ValidationFunc => {
+    if (CURRENT_AGE(val) <= AGE_REQUIREMENT) {
+      return 'A valid date input ensuring the user is above a 13';
+    }
+
+    return null;
+  };
+
+  const postalCodeValidation = (val: string): ValidationFunc => {
+    console.log(val);
+    return val;
+  };
+
+  const countryValidation = (val: string): ValidationFunc => {
+    console.log(val);
+
+    return val;
+  };
+
   const [opened, { toggle }] = useDisclosure(true);
 
   const [isBillingAddressChecked, setIsBillingAddressChecked] = useState<boolean>(false);
+
+  const addressValidation = {
+    country: (val: string) => countryValidation(val),
+    postalCode: (val: string) => postalCodeValidation(val),
+    city: (val: string) => stringValidation(val),
+    street: (val: string) => streetValidation(val),
+  };
 
   const form = useForm({
     initialValues: {
@@ -44,6 +132,22 @@ function Registration() {
         street: '',
       },
     },
+
+    validate: {
+      firstName: (val: string) => stringValidation(val),
+      lastName: (val: string) => stringValidation(val),
+      email: (val: string) => emailValidation(val),
+      password: (val: string) => passwordValidation(val),
+      dateOfBirthday: (val: string) => birthdayValidation(val),
+      shippingAddress: {
+        ...addressValidation,
+      },
+      billingAddress: {
+        ...addressValidation,
+      },
+    },
+
+    validateInputOnChange: true,
   });
 
   const billingSwitch = (
@@ -68,7 +172,7 @@ function Registration() {
       <Title align="center" color="orange" order={1} size="h1">
         Registration
       </Title>
-      <form>
+      <form onSubmit={form.onSubmit(console.log)}>
         <Flex direction="column" justify="center" gap={10}>
           <TextInput
             placeholder="Vasya"
@@ -102,23 +206,23 @@ function Registration() {
                 <TextInput
                   placeholder="Lenin st. 12-01"
                   label="Street"
-                  {...form.getInputProps('address.street')}
+                  {...form.getInputProps('shippingAddress.street')}
                 />
                 <TextInput
                   placeholder="Minsk"
                   label="City"
-                  {...form.getInputProps('address.city')}
-                />
-                <TextInput
-                  placeholder="AF-35A"
-                  label="Postal code"
-                  {...form.getInputProps('address.postalCode')}
+                  {...form.getInputProps('shippingAddress.city')}
                 />
                 <Select
                   placeholder="Belarus"
                   label="Country"
                   data={[]}
-                  {...form.getInputProps('address.country')}
+                  {...form.getInputProps('shippingAddress.country')}
+                />
+                <TextInput
+                  placeholder="AF-35A"
+                  label="Postal code"
+                  {...form.getInputProps('shippingAddress.postalCode')}
                 />
                 <Flex justify="space-between">
                   <Switch label="Set as default shipping address" />
@@ -148,16 +252,16 @@ function Registration() {
                   label="City"
                   {...form.getInputProps('billingAddress.city')}
                 />
-                <TextInput
-                  placeholder="AF-35A"
-                  label="Postal code"
-                  {...form.getInputProps('billingAddress.postalCode')}
-                />
                 <Select
                   placeholder="Belarus"
                   label="Country"
                   data={[]}
                   {...form.getInputProps('billingAddress.country')}
+                />
+                <TextInput
+                  placeholder="AF-35A"
+                  label="Postal code"
+                  {...form.getInputProps('billingAddress.postalCode')}
                 />
 
                 {opened && billingSwitch}
