bc2be8684931aa03ffe5fa1d9510b22cc1c5e72f
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-15 22:10:56 +0300

    [EA-87] feat: add render with providers testing functions

---
+++ src/test/index.tsx
@@ -0,0 +1,63 @@
+import { RenderOptions, render, renderHook } from '@testing-library/react';
+import { PreloadedState } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { PropsWithChildren } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { setupStore } from '../store';
+import type { RootState, AppStore } from '../store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {
+      user: {
+        user: null,
+        loading: false,
+        error: '',
+      },
+    },
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) {
+  function Wrapper({ children }: PropsWithChildren) {
+    return (
+      <BrowserRouter>
+        <Provider store={store}>{children}</Provider>
+      </BrowserRouter>
+    );
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
+
+export function renderHookWithProviders<Result, Props = undefined>(
+  callback: (initialProps: Props) => Result,
+  {
+    preloadedState = {
+      user: {
+        user: null,
+        loading: false,
+        error: '',
+      },
+    },
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) {
+  function Wrapper({ children }: PropsWithChildren) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return {
+    store,
+    ...renderHook<Result, Props>(callback, {
+      wrapper: Wrapper,
+      ...renderOptions,
+    }),
+  };
+}
