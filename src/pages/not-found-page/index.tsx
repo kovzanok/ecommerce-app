ad6f7d798226db650de5508d99125c6a933be1e7
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-17 15:14:00 +0300

    [EA-92] feat: add responsive styles for not found page

--- src/pages/not-found-page/index.tsx
+++ src/pages/not-found-page/index.tsx
@@ -1,5 +1,5 @@
 import {
-  Text, Title, Image, Flex, Container, Button,
+  Text, Title, Flex, Container, Button,
 } from '@mantine/core';
 import { NavLink } from 'react-router-dom';
 import imageSrc from '../../assets/not-found.png';
@@ -7,9 +7,9 @@ import imageSrc from '../../assets/not-found.png';
 export default function NotFoundPage() {
   return (
     <Container>
-      <Flex columnGap={20} align="center" justify="space-between">
+      <Flex wrap="wrap" columnGap={10} align="center" justify="space-evenly">
         <Flex align="center" rowGap={20} direction="column">
-          <Title ta="center" size={40}>
+          <Title ta="center" size={24}>
             Whoops, that page&apos;s gone.
           </Title>
           <Text size={18} ta="center">
@@ -28,8 +28,15 @@ export default function NotFoundPage() {
             </NavLink>
           </Button>
         </Flex>
-
-        <Image width={600} height={500} src={imageSrc} />
+        <img
+          style={{
+            minWidth: '300px',
+          }}
+          width="55%"
+          height="100%"
+          src={imageSrc}
+          alt="Not found"
+        />
       </Flex>
     </Container>
   );
