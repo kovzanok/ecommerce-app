bfc42709e3da74fef184701c2bdd62e72e13b9e5
Alex <105801205+AlexeiIsProgrammer@users.noreply.github.com>
2023-08-18 22:03:37 +0300

    [EA-85] Test validation functions used in register page (#59)
    * [EA-85] feat: add tests to registration validation functions
    
    * [EA-85] refactor: rename it-cases to readable format
    
    * [EA-85] refactor: rename it-case to readable format

---
+++ src/utils/field-validation/index.test.ts
@@ -0,0 +1,82 @@
+import { describe, it } from 'vitest';
+import {
+  validateBirthday,
+  validatePostalCode,
+  validateStreet,
+  validateString,
+} from '.';
+
+describe('validateString', () => {
+  it('should return error, if field is empty', () => {
+    const actual = '';
+
+    expect(validateString(actual)).toBe(
+      'This field must contain at least one character',
+    );
+  });
+  it('should return error, if field has special characters', () => {
+    const actual = 'St$reet';
+
+    expect(validateString(actual)).toBe(
+      'This field must contain no special characters or numbers',
+    );
+  });
+  it('should pass validation', () => {
+    const actual = 'Alexei';
+
+    expect(validateString(actual)).toBe(undefined);
+  });
+});
+
+describe('validateStreet', () => {
+  it('should return error, if field is empty', () => {
+    const actual = '';
+
+    expect(validateStreet(actual)).toBe(
+      'This field must contain at least one character',
+    );
+  });
+
+  it('should pass validation', () => {
+    const actual = 'Baker street';
+
+    expect(validateStreet(actual)).toBe(undefined);
+  });
+});
+
+describe('validateBirthday', () => {
+  it('should return error, if field is empty', () => {
+    const actual = '';
+
+    expect(validateBirthday(actual)).toBe('Choose your age');
+  });
+  it('should return error, if user is younger then 13', () => {
+    const actual = '2020-12-12';
+
+    expect(validateBirthday(actual)).toBe(
+      'A valid date input ensuring the user is above a 13',
+    );
+  });
+  it('should pass validation', () => {
+    const actual = '2000-12-12';
+
+    expect(validateBirthday(actual)).toBe(undefined);
+  });
+});
+
+describe('validatePostalCode', () => {
+  it('should return error after incorrect postal code', () => {
+    const actualPostalCode = '123456';
+    const actualCountry = 'US';
+
+    expect(validatePostalCode(actualPostalCode, actualCountry)).toBe(
+      'This postal code is incorrect!',
+    );
+  });
+  it('should pass validation', () => {
+    const actualPostalCode = '12345';
+    const actualCountry = 'US';
+
+    expect(validatePostalCode(actualPostalCode, actualCountry)).toBe(undefined);
+  });
+});
