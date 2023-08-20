662c7cd825f75f02fd27c77d63d3f58698d50dbc
theNickola <110473056+theNickola@users.noreply.github.com>
2023-08-20 11:51:50 +0300

    [EA-84] Test validation functions used in Login page (#60)
    * [EA-84] feat: add test cases for email validation
    
    * [EA-84] feat: add tests for password validation

--- src/utils/field-validation/index.test.ts
+++ src/utils/field-validation/index.test.ts
@@ -1,6 +1,8 @@
 import { describe, it } from 'vitest';
 import {
   validateBirthday,
+  validateEmail,
+  validatePassword,
   validatePostalCode,
   validateStreet,
   validateString,
@@ -78,5 +80,80 @@ describe('validatePostalCode', () => {
     const actualCountry = 'US';
 
     expect(validatePostalCode(actualPostalCode, actualCountry)).toBe(undefined);
+  });
+});
+
+describe('validateEmail', () => {
+  const testCases = [
+    {
+      name: 'should return error for email containing leading or trailing spaces',
+      inValue: ' test@test.com',
+      expectedValue:
+        'Email address must not contain leading or trailing whitespace',
+    },
+    {
+      name: 'should return error for email not containing an "@" symbol separating local part and domain name',
+      inValue: 'testtest.com',
+      expectedValue:
+        'Email address must contain an "@" symbol separating local part and domain name',
+    },
+    {
+      name: 'should return error for email not containing a domain name',
+      inValue: 'test@test',
+      expectedValue: 'Email address must contain a domain name',
+    },
+    {
+      name: 'should return error for email not properly formatted',
+      inValue: 'test test@test.com',
+      expectedValue: 'Email address must be properly formatted',
+    },
+  ];
+  testCases.forEach((test) => {
+    it(test.name, () => {
+      expect(validateEmail(test.inValue)).toBe(test.expectedValue);
+    });
+  });
+});
+
+describe('validatePassword', () => {
+  const testCases = [
+    {
+      name: 'should return error for password length less than 8',
+      inValue: 'Aa1 @',
+      expectedValue: 'Password must be at least 8 characters long',
+    },
+    {
+      name: 'should return error for password containing leading or trailing spaces',
+      inValue: 'Test1234@ ',
+      expectedValue: 'Password must not contain leading or trailing whitespace',
+    },
+    {
+      name: 'should return error for password not containing at least one uppercase letter (A-Z)',
+      inValue: 'test123@',
+      expectedValue:
+        'Password must contain at least one uppercase letter (A-Z)',
+    },
+    {
+      name: 'should return error for password not containing at least one lowercase letter (a-z)',
+      inValue: 'TEST123#',
+      expectedValue:
+        'Password must contain at least one lowercase letter (a-z)',
+    },
+    {
+      name: 'should return error for password not containing at least one digit (0-9)',
+      inValue: 'TestTest#@',
+      expectedValue: 'Password must contain at least one digit (0-9)',
+    },
+    {
+      name: 'should return error for password not containing at least one special character (e.g., !@#$%^&*)',
+      inValue: 'Test1234',
+      expectedValue:
+        'Password must contain at least one special character (e.g., !@#$%^&*)',
+    },
+  ];
+  testCases.forEach((test) => {
+    it(test.name, () => {
+      expect(validatePassword(test.inValue)).toBe(test.expectedValue);
+    });
   });
 });
