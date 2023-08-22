727c1f95d7cc41d5b9cf7055d710025e38883a64
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-22 18:14:34 +0300

    [EA-64] feat: add access to the page only to logged-in users

--- src/pages/user-page/index.tsx
+++ src/pages/user-page/index.tsx
@@ -1,3 +1,9 @@
+import { Navigate } from 'react-router-dom';
+import { useAppSelector } from '../../hooks';
+import userSelector from '../../store/selectors';
+
 export default function UserPage() {
+  const { user } = useAppSelector(userSelector);
+  if (!user) return <Navigate to="/login" />;
   return <div>UserPage</div>;
 }
