ab3d43dc866b821a1f1492141d8cc5997c24fa6e
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-23 18:50:36 +0300

    [EA-45] feat: fetch and display product list

--- src/pages/catalog-page/index.tsx
+++ src/pages/catalog-page/index.tsx
@@ -1,3 +1,45 @@
+import {
+  Loader, Flex, Center, Title,
+} from '@mantine/core';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { productsSelector } from '../../store/selectors';
+import { fetchProducts } from '../../store/slices/productsSlice';
+import ProductCard from '../../components/product-card';
+
 export default function CatalogPage() {
-  return <div>CatalogPage</div>;
+  const { products, loading } = useAppSelector(productsSelector);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  if (loading) {
+    return (
+      <Center h="100%">
+        <Loader mt={100} size={100} color="orange" />
+      </Center>
+    );
+  }
+  if (!products?.length) {
+    return (
+      <Title ta="center" order={4}>
+        No products
+      </Title>
+    );
+  }
+  return (
+    <Flex
+      rowGap={5}
+      columnGap={5}
+      align="start"
+      justify="space-evenly"
+      wrap="wrap"
+    >
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </Flex>
+  );
 }
