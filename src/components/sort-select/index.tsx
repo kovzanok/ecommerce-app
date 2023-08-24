a700060fa9592818db3982e7aca4933071026af3
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-24 20:14:43 +0300

    [EA-48] feat: add sort select

---
+++ src/components/sort-select/index.tsx
@@ -0,0 +1,30 @@
+import { Select } from '@mantine/core';
+import { Sorting } from '../../types';
+
+type SortSelectProps = {
+  loading: boolean;
+  value: Sorting;
+  handleChange: (value: string | null) => void;
+};
+
+export default function SortSelect({
+  loading,
+  value,
+  handleChange,
+}: SortSelectProps) {
+  const sortingOptions: { value: Sorting; label: string }[] = [
+    { value: 'name.en-US asc', label: 'A-Z' },
+    { value: 'name.en-US desc', label: 'Z-A' },
+    { value: 'price asc', label: 'Price ↑' },
+    { value: 'price desc', label: 'Price ↓' },
+  ];
+  return (
+    <Select
+      disabled={loading}
+      onChange={handleChange}
+      value={value}
+      data={sortingOptions}
+      label="Sorting"
+    />
+  );
+}
