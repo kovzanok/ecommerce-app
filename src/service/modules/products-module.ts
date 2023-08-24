d5eb890bea6b773f6b9584ad1d34400fcde45c8d
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-24 13:56:25 +0300

    [EA-47] feat: add filters to api request

--- src/service/modules/products-module.ts
+++ src/service/modules/products-module.ts
@@ -4,21 +4,24 @@ import {
 } from '@commercetools/platform-sdk';
 import BaseModule from '../../utils/base-module';
 import { ProductsQuery } from '../../types';
+import { createQueryString } from '../../utils';
 
 export default class ProductsModule extends BaseModule {
   static productTypeId = 'aa10f723-a1bc-4131-975d-f26f9174ecf3';
 
   static async getProducts({
     search,
+    filters,
   }: ProductsQuery): Promise<ProductProjection[] | undefined> {
     try {
+      const queryString = createQueryString(filters);
       const {
         body: { results },
       } = await ProductsModule.apiRoot
         .productProjections()
         .search()
         .get({
-          queryArgs: { 'text.en-US': search },
+          queryArgs: { 'text.en-US': search, 'filter.query': queryString },
         })
         .execute();
       return results;
