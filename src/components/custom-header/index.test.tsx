c662ad6e628a275cb020b22056abd349851f42ba
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-25 19:15:21 +0300

    [EA-112] feat: update tests

--- src/components/custom-header/index.test.tsx
+++ src/components/custom-header/index.test.tsx
@@ -41,4 +41,38 @@ describe('CustomHeader', () => {
     renderWithProviders(<CustomHeader />, { preloadedState });
     expect(await screen.findByRole('button')).toHaveTextContent('Logout');
   });
+  it('should render link to catalog page', async () => {
+    renderWithProviders(<CustomHeader />);
+    expect(await screen.findByText('Catalog')).toHaveTextContent('Catalog');
+  });
+  it('should render link to profile page to authorized users', async () => {
+    const preloadedState: RootState = {
+      user: {
+        user: {
+          customer: {
+            addresses: [],
+            email: 'johndoe@example.com',
+            firstName: 'John',
+            id: 'some_123_id',
+            isEmailVerified: false,
+            lastName: 'Doe',
+            password: '****aGg=',
+            version: 1,
+            createdAt: '2015-07-06T13:22:33.339Z',
+            lastModifiedAt: '2015-07-06T13:22:33.339Z',
+            authenticationMode: 'Password',
+          },
+        },
+        loading: false,
+        error: '',
+      },
+      products: {
+        products: [],
+        loading: false,
+        error: '',
+      },
+    };
+    renderWithProviders(<CustomHeader />, { preloadedState });
+    expect(await screen.findByText('Profile')).toHaveTextContent('Profile');
+  });
 });
