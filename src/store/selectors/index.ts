d0bff80e3389a9290d16cc2608ef452a9b0b32a2
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-26 13:42:41 +0300

    [EA-53] feat: add product selector

--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -2,5 +2,6 @@ import { RootState } from '..';
 
 const userSelector = (state: RootState) => state.user;
 export const productsSelector = (state: RootState) => state.products;
+export const productSelector = (state: RootState) => state.product;
 
 export default userSelector;
