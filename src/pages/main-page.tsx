92cbd0a51ff5fd6260cbb8ddaa3c0152cf20576f
Alex Kovzan <alexanderkovzan@gmail.com>
2023-09-01 00:47:09 +0300

    [EA-155] feat: add filters and sorting options to main page

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,4 +1,6 @@
-import { Flex, Title } from '@mantine/core';
+import {
+  Flex, Title, Text, Badge,
+} from '@mantine/core';
 import { PageLink } from '../types';
 import PageCard from '../components/page-card';
 import { useTitle } from '../hooks';
@@ -37,16 +39,76 @@ export default function MainPage() {
         'User Profile page is available only for authorized users and contains all ther user information, which can be modified and saved.',
     },
   ];
+
+  const sortingOptions: string[] = [
+    'by name in alphabetical order',
+    'by name in reverse alphabetical order',
+    'by price in ascending order',
+    'by price in descending order',
+  ];
+
+  const filters: string[] = [
+    'by author',
+    'by cover type',
+    'by age',
+    'by publisher',
+    'by price',
+  ];
+
   return (
-    <>
-      <Title ta="center" mb={20} size={24}>
-        Available pages
-      </Title>
-      <Flex rowGap={20} wrap="wrap" justify="space-evenly">
-        {links.map((pageLink) => (
-          <PageCard key={pageLink.to} {...pageLink} />
-        ))}
+    <Flex direction="column" rowGap={20}>
+      <div>
+        {' '}
+        <Title ta="center" mb={20} size={24}>
+          Available pages
+        </Title>
+        <Flex rowGap={20} wrap="wrap" justify="space-evenly">
+          {links.map((pageLink) => (
+            <PageCard key={pageLink.to} {...pageLink} />
+          ))}
+        </Flex>
+      </div>
+      <Flex justify="space-evenly" wrap="wrap" rowGap={30}>
+        <div>
+          <Title ta="center" mb={5} size={24}>
+            Available filters
+          </Title>
+          <Text ta="center">
+            User can filter products on catalog page with provided filters:
+            <Flex
+              justify="space-evenly"
+              rowGap={10}
+              mt={10}
+              wrap="wrap"
+              columnGap={20}
+            >
+              {filters.map((option) => (
+                <Badge color="orange">{option}</Badge>
+              ))}
+            </Flex>
+          </Text>
+        </div>
+        <div>
+          <Title ta="center" mb={5} size={24}>
+            Available sorting options
+          </Title>
+          <Text ta="center">
+            User can sort products on catalog page with provided sorting
+            options:
+            <Flex
+              justify="space-evenly"
+              rowGap={10}
+              mt={10}
+              wrap="wrap"
+              columnGap={20}
+            >
+              {sortingOptions.map((option) => (
+                <Badge color="orange">{option}</Badge>
+              ))}
+            </Flex>
+          </Text>
+        </div>
       </Flex>
-    </>
+    </Flex>
   );
 }
