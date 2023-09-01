c1e4c1628f32d066c0f44c5764eff6bb15d86541
Alex Kovzan <alexanderkovzan@gmail.com>
2023-09-01 09:41:32 +0300

    [EA-153] fix: fix addresses page tests

--- src/pages/addresses-user-page/index.test.tsx
+++ src/pages/addresses-user-page/index.test.tsx
@@ -19,7 +19,34 @@ describe('AddressesUserPage', () => {
   });
 
   it('should render page with submit button', async () => {
-    renderWithProviders(<AddressesUserPage />);
+    const user = {
+      user: {
+        customer: {
+          addresses: [],
+          email: 'johndoe@example.com',
+          firstName: 'John',
+          id: 'some_123_id',
+          isEmailVerified: false,
+          lastName: 'Doe',
+          password: '****aGg=',
+          version: 1,
+          createdAt: '2015-07-06T13:22:33.339Z',
+          lastModifiedAt: '2015-07-06T13:22:33.339Z',
+          authenticationMode: 'Password',
+        },
+      },
+      loading: false,
+      error: '',
+    };
+    const products = {
+      products: [],
+      loading: false,
+      error: '',
+    };
+    const product = { product: null, loading: false, error: '' };
+    renderWithProviders(<AddressesUserPage />, {
+      preloadedState: { user, product, products },
+    });
     const button: HTMLElement = await screen.findByRole('button');
     expect(button).toBeInTheDocument();
     expect(button).toHaveTextContent('Add address');
