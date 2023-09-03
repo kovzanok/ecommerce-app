40b42ee8bdbffe857f83090a1c44d75dbec71486
Alex Kovzan <alexanderkovzan@gmail.com>
2023-09-03 13:36:17 +0300

    [EA-154] feat: add cart selector

--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -3,4 +3,5 @@ import { RootState } from '..';
 const userSelector = (state: RootState) => state.user;
 export const productsSelector = (state: RootState) => state.products;
 export const productSelector = (state: RootState) => state.product;
+export const cartSelector = (state: RootState) => state.cart;
 export default userSelector;
