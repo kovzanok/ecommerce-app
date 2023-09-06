6cab139662ca0cb312aa88a7605e8b67de541051
Alexei <avenge.now.r@mail.ru>
2023-09-06 18:43:11 +0300

    [EA-132] feat: add empty cart message

--- src/components/cart-list/index.tsx
+++ src/components/cart-list/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
-  Center, Flex, Loader, Paper, Title,
+  Box, Center, Flex, Loader, Paper, Text, Title,
 } from '@mantine/core';
 import { cartSelector } from '../../store/selectors';
 import CartItem from '../cart-item';
 import CartPagination from '../cart-pagination';
+import emptyCart from '../../assets/empty-cart.jpg';
 import { PaginationType } from '../../types';
 import { calculatePagination } from '../../utils';
 
@@ -31,6 +32,23 @@ function CartList() {
         <Center h="100%">
           <Title ta="center">{error}</Title>
         </Center>
+      );
+      break;
+    case cart === null || cart?.lineItems.length === 0:
+      content = (
+        <Box>
+          <img
+            style={{
+              minWidth: '300px',
+            }}
+            width="55%"
+            height="100%"
+            src={emptyCart}
+            alt="No items"
+          />
+
+          <Text>Basket is empty</Text>
+        </Box>
       );
       break;
     default:
